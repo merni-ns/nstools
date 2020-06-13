b8dfb5146e5d14a42ec7b8c6a988445541548ff7
merni-ns <66267867+merni-ns@users.noreply.github.com>
2020-06-13 17:17:13 +0530

    add rmb v4

--- rmb.py
+++ rmb.py
@@ -1,5 +1,5 @@
 #RMB Archiving script by Merni
-#Version 3
+#Version 4
 #If the script fails, send me a TG.
 #Automatically ratelimited to 100 msgs (1 req) every 1.5s, i.e. 20 reqs / 30s
 #Some code using urllib taken from stackoverflow.
@@ -9,7 +9,8 @@ import xml.etree.ElementTree as ET
 from copy import copy
 from datetime import datetime
 import time
- 
+import argparse
+
 def dictify(r,root=True):
     #This function by Erik Aronesty at stackoverflow
     if root:
@@ -22,20 +23,39 @@ def dictify(r,root=True):
             d[x.tag] = []
         d[x.tag].append(dictify(x,False))
     return d
- 
-print('Your useragent should have your email/NS name, as well as the purpose of this program (RMB archiving). The mods need this info.')
-useragent = input('Enter your useragent : ')
-regionname = input('Enter your region name : ')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--useragent", help="NS API user agent")
+parser.add_argument("-r", "--region", help="Region name")
+parser.add_argument("-o", "--file", help="File to output the result (with path if required)")
+parser.add_argument("-i", "--startid", help="Starting Post ID")
+parser.add_argument("-s", "--startno", help="Starting serial number")
+args = parser.parse_args()
+
+if None in (args.region, args.useragent, args.file):
+    print('Your useragent should have your email/NS name, as well as the purpose of this program (RMB archiving). The mods need this info.')
+    useragent = input('Enter your useragent : ')
+    regionname = input('Enter your region name : ')
+    print('You must enter the full path of the text file, including extension. If the file is in your Python directory, you can enter the filename and extension only. If the file does not exist it will be created.')
+    filename = input('Enter path/name of text file : ')
+    adv = input('Advanced options? Y/N : ').casefold()
+    if adv =='y':
+        print('>>>If you want to start from the first post, press Enter for the next two prompts.<<<')
+        print('The archive assigns a serial number 1,2,3... to each post. If you are starting from a later post,you may want to change the first serial number. Otherwise press Enter.')
+        sid = input('Enter starting post ID : ')
+        sn = input('Enter starting serial number : ')
+    else: sid = sn = ''
+else:
+    useragent = args.useragent
+    regionname = args.region
+    filename = args.file
+sid = '' if args.startid is None else args.startid
+sn = '' if args.startno is None else args.startno
+
+
 regionnamef = regionname.casefold().replace(' ','_')
-print('You must enter the full path of the text file, including extension. If the file is in your Python directory, you can enter the filename and extension only. If the file does not exist it will be created.')
-filename = input('Enter path/name of text file : ')
-adv = input('Advanced options? Y/N : ').casefold()
-if adv =='y':
-    print('>>>If you want to start from the first post, press Enter for the next two prompts.<<<')
-    print('The archive assigns a serial number 1,2,3... to each post. If you are starting from a later post,you may want to change the first serial number. Otherwise press Enter.')
-    sid = input('Enter starting post ID : ')
-    sn = input('Enter starting serial number : ')
-else: sid = sn = ''
+
+
  
 nt = datetime.utcnow().strftime('%Y-%m-%d at %H:%M:%S UTC')
 init = 1 if sn=='' else int(sn)
